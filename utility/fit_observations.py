48d131c66c24680fd530316cac4eaa369efdc7cd
Ekaterina Semenova <semenova@mpia.de>
2022-05-16 16:40:21 +0200

    added fitting using NN and convolving with resolution and macroturbulence for noth modes (models and NN)

--- utility/fit_observations.py
+++ utility/fit_observations.py
@@ -7,7 +7,9 @@ from scipy import interpolate
 from observations import readSpectrumTSwrapper, spectrum, read_observations
 from multiprocessing import Pool
 from scipy.optimize import curve_fit, fmin_bfgs
-
+import sys
+sys.path.append('/home/semenova/codes/PayneNN')
+from source.validationChecks import restore, readNN
 
 def readModelSpectralGrid(ListOfModelSpectra):
     "Assuming the same wavelength because there is too much going on with cutting the spectra and so on"
@@ -61,20 +63,24 @@ def getClosestModel(specObs, modelGrid, coords, labels):
         print(f"{k} = {labels[i]}")
         dist += ( modelGrid[k] - labels[i] )**2
     pos = np.where(dist == min(dist))[0]
-#    if len(pos) > 1:
-#        print(f"found {len(pos)} 'closest' spectra:")
-#        for p in pos:
-#            print(spectraGrid['path'][p].split('/')[-1])
+    if len(pos) > 1:
+       print(f"found {len(pos)} 'closest' spectra:")
+       for p in pos:
+           print(spectraGrid['path'][p].split('/')[-1])
     pos = pos[0]
     modSpec = readSpectrumTSwrapper( modelGrid['path'][pos] )
+    modSpec.convolve_resolution(specObs.R)
+    modSpec.convolve_macroturbulence(labels[-1])
+
     print(modelGrid['path'][pos].split('/')[-1])
     print()
+
     flux = np.interp(specObs.lam, modSpec.lam, modSpec.flux )
     chi2 = np.sqrt(np.sum( ( flux - specObs.flux ) ** 2 ))
 
     return chi2
 
-def fitObservedSpectrum(obsSpec, modelGrid, fitForLabels = None):
+def fitToModelGrid(obsSpec, modelGrid, fitForLabels = None):
 
     if isinstance(fitForLabels, type(None)):
         fitForLabels = modelGrid['coordinates']
@@ -84,51 +90,87 @@ def fitObservedSpectrum(obsSpec, modelGrid, fitForLabels = None):
     for k in fitForLabels:
         print(k, np.mean( modelGrid[k] ))
         initLabels.append( np.mean( modelGrid[k] ) )
+    " Add macro-turbulence "
+    initLabels.append(0)
     print(initLabels)
 
     fitFunc = lambda label : getClosestModel(obsSpec, modelGrid, fitForLabels, label)
-    epsilon = []
-    for k in fitForLabels:
-      epsilon.append(np.std(modelGrid[k])/1000)
-    popt = fmin_bfgs( fitFunc, initLabels, epsilon = epsilon)
-    
+    popt = fmin_bfgs( fitFunc, initLabels)
     print(popt)
-    #fitFunc = lambda lam, *label : getClosestModel(lam, modelGrid, fitForLabels, label)
-    #popt, pcov = curve_fit(
-    #                        fitFunc, obsSpec.lam,
-    #                        obsSpec.flux,
-    #                        p0 = initLabels,
-    #                        )
-    #bestFitFlux = getClosestModel(obsSpec.lam, modelGrid, fitForLabels, popt)
-    #print(popt)
-    #return popt, bestFitFlux
+    return popt
 
+
+def callNN(wavelength, obsSpec, NN, labels):
+    spec = spectrum(
+                    wavelength,
+                    restore(wavelength, NN, labels[:-2]), res = np.inf
+                    )
+    spec.convolve_resolution(obsSpec.R)
+    spec.convolve_macroturbulence(labels[-1])
+    return spec.flux
+
+def fitToNeuralNetwork(obsSpec, NN, fitForLabels = None):
+
+    if isinstance(fitForLabels, type(None)):
+        fitForLabels = NN['labelsKeys']
+
+    """
+    Initialise the labels if not provided
+    Extra dimension for macro-turbulence
+    """
+
+    initLabels = np.zeros(len(fitForLabels)+1)
+
+
+    fitFunc = lambda wavelength, labels : callNN(
+                                                wavelength, obsSpec,
+                                                NN, labels
+                                                )
+    popt = curve_fit(
+                    fitFunc, obsSpec.lam, \
+                    obsSpec.flux, p0=initLabels
+                    )
+
+    labels[:-2] = (popt[:-2] + 0.5)*( NNet['x_max'] - NNet['x_min'] ) + NNet['x_min']
+    return labels
 
 if __name__ == '__main__':
-    if len(argv) < 3:
+    if len(argv) < 4:
         print("Usage: $ python ./fit_observations.py \
-<path to model spectra> <path to observed spectra> ")
+<path to model spectra or payne NN> <path to observed spectra> <mode (grid or payne)>")
         exit()
-    modelPath = argv[1]
-    obsPath = argv[2]
 
-    modelGrid = gatherModelData(modelPath)
+    if argv[3].lower().strip() == 'grid':
+        "Fit to the grid"
 
-    fitForLabels = ['teff', 'logg', 'feh', 'vturb']
-    for obsSpecPath in glob.glob(obsPath):
-        w, f = read_observations(obsSpecPath, format = 'ascii')
-        obsSpec = spectrum(
-                        w, f,\
-                        res = np.inf
-                        )
-        fitObservedSpectrum(obsSpec, modelGrid, fitForLabels )
+        modelPath = argv[1]
+        obsPath = argv[2]
 
+        modelGrid = gatherModelData(modelPath)
 
+        fitForLabels = ['teff', 'logg', 'feh', 'vturb']
+        for obsSpecPath in glob.glob(obsPath):
+            w, f = read_observations(obsSpecPath, format = 'ascii')
+            obsSpec = spectrum(
+                            w, f,\
+                            res = np.inf
+                            )
+            labelsFit = fitToModelGrid(obsSpec, modelGrid, fitForLabels )
+    elif argv[3].lower().strip() == 'payne':
+        "Fit using Payne neural network"
+        nnPath = argv[1]
+        obsPath = argv[2]
 
+        NN = readNN(nnPath)
 
+        for obsSpecPath in glob.glob(obsPath):
+            w, f = read_observations(obsSpecPath, format = 'ascii')
+            obsSpec = spectrum(
+                            w, f,\
+                            res = np.inf
+                            )
+            labelsFit = fitToNeuralNetwork(obsSpec, NN)
 
-
-
-
-
-    exit()
+    else:
+        print(f"Mode {argv[4]} not understood. 'Grid' or 'Payne' are supported.")
+        exit()
