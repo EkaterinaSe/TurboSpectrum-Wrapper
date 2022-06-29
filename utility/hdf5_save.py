7234bd59fbb6c17d3cdb20a276914f8dc6e1470c
Ekaterina Semenova <semenova@mpia.de>
2022-06-29 14:28:16 +0200

    debug

--- utility/hdf5_save.py
+++ utility/hdf5_save.py
@@ -4,43 +4,47 @@ from multiprocessing import Pool
 import glob
 from observations import readSpectrumTSwrapper
 import h5py
+import cProfile
+import pstats
 
 def gather_data(specList):
     # spectraList, wave_new, Rnew, quite, limits = arg
-    flxl = []
     spectraIncl = []
     # get the labels
     spec = readSpectrumTSwrapper(specList[0])
+    wvl = spec.lam
     labels = { k:[] for k in spec.labels}
+    flxl = np.zeros( shape=(len(specList), len(spec.lam) ) )
 
-    for specFile in specList:
+    for i, specFile in enumerate(specList):
         spec = readSpectrumTSwrapper(specFile)
-        flxl.append(spec.flux)
+        flxl[i] = spec.flux
         for k in labels:
             labels[k].append(spec.__dict__[k])
-    print(flxl)
-    print(labels)
-    return flxl, labels
+    return flxl, labels, wvl
 
 if __name__ == '__main__':
 
     path = argv[1]
-    specList = glob.glob(path)[:10]
-    gather_data(specList)
+    specList = glob.glob(path)[:100]
+
+    profiler = cProfile.Profile()
+    profiler.enable()
+
+    flxl, labels, wvl = gather_data(specList)
 
     with h5py.File('./test.h5', 'w') as hf:
-        hf.creat_dataset( 'fluxes', flxl )
-        hf.creat_dataset( 'labels', labels )
+        hf.create_dataset( 'fluxes', data=flxl, shape=np.shape(flxl), dtype='float64')
+        for k in labels:
+            hf.create_dataset( f"{k}", data=labels[k] )
+        hf.create_dataset( 'wave', data=wvl, shape=np.shape(wvl), dtype='float64')
 
 
 
-
-    with open('./test.h5', 'r') as hf:
-        print(hf.keys())
-        print(hf['labels'])
-
-
-
+    with h5py.File('./test.h5', 'r') as hf:
+        print( list(hf.keys()) )
+    stats = pstats.Stats(profiler).sort_stats('cumulative')
+    stats.print_stats()
 
 
 
