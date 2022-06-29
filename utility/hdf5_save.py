b4179e01338e38a8bcf60baf8814d6adc1e8f43d
Ekaterina Semenova <semenova@mpia.de>
2022-06-29 14:48:50 +0200

    parallel reading

--- utility/hdf5_save.py
+++ utility/hdf5_save.py
@@ -6,6 +6,7 @@ from observations import readSpectrumTSwrapper
 import h5py
 import cProfile
 import pstats
+from multiprocessing import Pool
 
 def gather_data(specList):
     # spectraList, wave_new, Rnew, quite, limits = arg
@@ -21,17 +22,30 @@ def gather_data(specList):
         flxl[i] = spec.flux
         for k in labels:
             labels[k].append(spec.__dict__[k])
-    return flxl, labels, wvl
+    return (flxl, labels, wvl)
 
 if __name__ == '__main__':
 
     path = argv[1]
+    if len(argv) > 2:
+        ncpu = int(argv[2])
+    else:
+        ncpu = 1
     specList = glob.glob(path)[:100]
 
-    profiler = cProfile.Profile()
-    profiler.enable()
+    # profiler = cProfile.Profile()
+    # profiler.enable()
 
-    flxl, labels, wvl = gather_data(specList)
+    args = [ spectraList[i::ncpu] for i in range(ncpu)]
+    with Pool(processes=ncpu) as pool:
+        out = pool.map(gather_data, args )
+    # for i in range(len(out)):
+    #     fl = out[i][0]
+    #     lab   = out[i][1]
+        # w   = out[i][1]
+    flxl = np.hstack( (out[i][0] for i in range(len(out))) )
+    print( np.shape(flxl) )
+    # flxl, labels, wvl = gather_data(specList)
 
     with h5py.File('./test.h5', 'w') as hf:
         hf.create_dataset( 'fluxes', data=flxl, shape=np.shape(flxl), dtype='float64')
@@ -41,10 +55,10 @@ if __name__ == '__main__':
 
 
 
-    with h5py.File('./test.h5', 'r') as hf:
-        print( list(hf.keys()) )
-    stats = pstats.Stats(profiler).sort_stats('cumulative')
-    stats.print_stats()
+    # with h5py.File('./test.h5', 'r') as hf:
+    #     print( list(hf.keys()) )
+    # stats = pstats.Stats(profiler).sort_stats('cumulative')
+    # stats.print_stats()
 
 
 
